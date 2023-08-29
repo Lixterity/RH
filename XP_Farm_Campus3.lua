3192d065e62a81e0ecb4a8eaa36caf31f2be7e0f
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-08-29 23:15:39 +0200

    Update XP_Farm_Campus3.lua

--- XP_Farm_Campus3.lua
+++ XP_Farm_Campus3.lua
@@ -2,9 +2,9 @@ repeat task.wait() until game:IsLoaded()
 local _ = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
 
 local scripts = {
-    ["735030788"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/TeleportMenu.lua?token=GHSAT0AAAAAACG5G2YO5WPLX2TEO4FZ34POZHOL4SQ"),
-    ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/Apartment.lua?token=GHSAT0AAAAAACG5G2YPF3W6OPU6X5NN5DGKZHOL4SA"),
-    ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/School.lua?token=GHSAT0AAAAAACG5G2YPST2BOQFFYFCZHWROZHOL4SA")
+    ["735030788"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/TeleportMenu.lua"),
+    ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/Apartment.lua"),
+    ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/School.lua")
 }; coroutine.resume(coroutine.create(loadstring(scripts[tostring(game.PlaceId)])))
     
 if game.PlaceId ~= 735030788 then 
