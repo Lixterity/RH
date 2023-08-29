51db078388742dd18b7d7c443bd5bee8bb8698af
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-08-30 00:21:34 +0200

    Update XP_Farm_Campus2.lua

--- XP_Farm_Campus2.lua
+++ XP_Farm_Campus2.lua
@@ -4,7 +4,7 @@ local _ = game:GetService("Players").LocalPlayer.Character or game:GetService("P
 local scripts = {
     ["735030788"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/TeleportAppartment.lua"),
     ["5589764519"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/TeleportMenu.lua"),
-    ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/Apartment.lua"),
+    ["4709927839"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/Apartment.lua"),
     ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/School.lua")
 }; coroutine.resume(coroutine.create(loadstring(scripts[tostring(game.PlaceId)])))
     
