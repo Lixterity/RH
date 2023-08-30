e82b54560d3fc039168d4dc72a6caf3ecb1d27f9
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-08-30 03:26:08 +0200

    Update Apartment.lua

--- Apartment.lua
+++ Apartment.lua
@@ -12,7 +12,7 @@ local function sleep()
     
     repeat task.wait(2) 
     until getupvalues(getconnections(energyEventHandler)[2].Function)[1] == 1
-    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("Model"):WaitForChild("ElevatorLockDoor").Position)
+    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("KitchenLightSwitch"):WaitForChild("Switch").Position)
     task.wait(2)
     services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
 end; sleep()
