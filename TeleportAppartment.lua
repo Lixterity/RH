b5c1dbbc413dfc706821bdb3920bea497dc59f9d
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-08-30 02:24:58 +0200

    Update TeleportAppartment.lua

--- TeleportAppartment.lua
+++ TeleportAppartment.lua
@@ -1,4 +1,3 @@
-repeat task.wait() until game:IsLoaded()
 game:GetService("ReplicatedStorage").TPScepterNetwork.StartTeleport:FireServer("ApartmentsLobby", 2)
 task.wait(10)
 game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.E,false,game)
