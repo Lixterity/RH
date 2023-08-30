06626efe145a3f5f6e488f8b92abd403832b5ea3
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-08-30 04:51:50 +0200

    Update TeleportAppartment.lua

--- TeleportAppartment.lua
+++ TeleportAppartment.lua
@@ -1,3 +1,3 @@
 game:GetService("ReplicatedStorage").TPScepterNetwork.StartTeleport:FireServer("ApartmentsLobby", 2)
-task.wait(5)
+task.wait(10)
 FireProximityPrompt(game:GetService("Workspace").TeleportPortals:GetChildren()[1].PromptAttachment.ProximityPrompt)
