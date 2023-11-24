0629b6f0559069a353b83c911069670dfef6f9fb
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-11-24 22:45:03 +0100

    Update School.lua

--- School.lua
+++ School.lua
@@ -1,13 +1,32 @@
+getgenv().Settings = {
+    DisableRendering = true,
+    FPSCap = 30
+} 
+
+setfpscap(Settings.FPSCap or 60)
+local tdrender = not Settings.DisableRendering
+RunService:Set3dRenderingEnabled(tdrender)
+
+local ScreenGui = Instance.new("ScreenGui", game.CoreGui)
+    local btn = Instance.new("TextButton", ScreenGui)
+
+    btn.BackgroundColor3 = Color3.fromRGB(18, 21, 28)
+    btn.BorderSizePixel = 0
+    btn.Text = "3D Render"
+    btn.TextColor3 = Color3.new(255, 255, 255)
+    btn.TextScaled = true
+    btn.Size = UDim2.new(0, 150, 0, 50)
+    btn.Position = UDim2.new(0.3, 0, 0.3, 0)
+    btn.MouseButton1Click:Connect(function()
+        tdrender = not tdrender
+        RunService:Set3dRenderingEnabled(tdrender)
+    end)
+
 local services = setmetatable({},{__index = function(_,serv) return game:GetService(serv) end})
 local classRemotes = services.ReplicatedStorage.Classes
 local prevConnections = {}
 
 local localPlayer = services.Players.LocalPlayer
-
-getgenv().Settings = {
-    DisableRendering = true,
-    FPSCap = 30
-} 
 
 function fireBack(remote, times, ...)
     local args = {...}
