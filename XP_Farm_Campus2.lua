f3410a089c6844736ce027ee58ba057455844f4d
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-11-25 13:38:48 +0100

    Update XP_Farm_Campus2.lua

--- XP_Farm_Campus2.lua
+++ XP_Farm_Campus2.lua
@@ -1,3 +1,5 @@
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
 repeat task.wait() until game:IsLoaded()
 
 local _ = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
