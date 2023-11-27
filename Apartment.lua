edb2a558d7d372d51eb933d3dada8b41bd07d4a8
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-11-27 19:06:55 +0100

    Update BeachHouse.lua

--- Apartment.lua
+++ Apartment.lua
@@ -5,16 +5,17 @@ local playerGui = localPlayer:WaitForChild("PlayerGui")
 local function sleep()
     local needsScreen = playerGui:WaitForChild("NeedsBar")
     local energyEventHandler = services.ReplicatedStorage.Needs.UpdateEnergy.OnClientEvent
+    
+    services.ReplicatedStorage.ToolGiverRemotes.GiveTool:FireServer("Sleeping Bag"); task.wait(2)
+    localPlayer.Backpack:WaitForChild("Sleeping Bag").Parent = localPlayer.Character; task.wait(2)
+    services.ReplicatedStorage.Tools.PlaceTool:FireServer(
+        localPlayer.Character:FindFirstChild("Sleeping Bag"),
+        localPlayer.Character.HumanoidRootPart.CFrame - Vector3.new(0,2,3.5),
+        {}
+    ); task.wait(2)
+    services.ReplicatedStorage.Bed.Anim:FireServer("sleep")
 
-    localPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("KitchenLightSwitch"):WaitForChild("Switch").Position)
-    
-    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("bed"):WaitForChild("BedSeat").Position)
-    task.wait(1)   
-    game:GetService("ReplicatedStorage").Bed.Anim:FireServer("Sleep", "All Tucked In")
-    
     repeat task.wait(2) 
     until getupvalues(getconnections(energyEventHandler)[2].Function)[1] == 1
-    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("KitchenLightSwitch"):WaitForChild("Switch").Position)
-    task.wait(2)
     services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
 end; sleep()
