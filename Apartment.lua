d918c60e74f1ce0282f661cdf799dc73a3cc6cdb
Lixterity <143555197+Lixterity@users.noreply.github.com>
2023-11-27 18:13:43 +0100

    Update Apartment.lua

--- Apartment.lua
+++ Apartment.lua
@@ -5,19 +5,16 @@ local playerGui = localPlayer:WaitForChild("PlayerGui")
 local function sleep()
     local needsScreen = playerGui:WaitForChild("NeedsBar")
     local energyEventHandler = services.ReplicatedStorage.Needs.UpdateEnergy.OnClientEvent
-    
-    localPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("bed"):WaitForChild("BedSeat").Position)
-    
-    repeat task.wait(5)
-        services.ReplicatedStorage.Bed.Anim:FireServer("Sleep", "All Tucked In")
-        print("sleeping")
-    until getupvalues(getconnections(energyEventHandler)[2].Function)[1] == 1
 
-    localPlayer.Character.Humanoid.Jump = true
     localPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("KitchenLightSwitch"):WaitForChild("Switch").Position)
     
-    repeat task.wait(5)
-        services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
-        print("tping")
-    until game.PlaceId == 1765700510
+    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("bed"):WaitForChild("BedSeat").Position)
+    task.wait(1)   
+    game:GetService("ReplicatedStorage").Bed.Anim:FireServer("Sleep", "All Tucked In")
+    
+    repeat task.wait(2) 
+    until getupvalues(getconnections(energyEventHandler)[2].Function)[1] == 1
+    game.Players.LocalPlayer.Character:MoveTo(workspace:WaitForChild("Apartments"):GetChildren()[1]:WaitForChild("ApartmentBuilding"):WaitForChild("Stuff"):WaitForChild("KitchenLightSwitch"):WaitForChild("Switch").Position)
+    task.wait(2)
+    services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
 end; sleep()
