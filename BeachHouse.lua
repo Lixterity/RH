30699dfbcf4a72b7a5a5bb2eb124959edbc08ce0
Aeroxx12 <136855637+Aeroxx12@users.noreply.github.com>
2023-12-03 12:54:05 +0100

    Update BeachHouse.lua

--- BeachHouse.lua
+++ BeachHouse.lua
@@ -16,18 +16,23 @@ local function sleep()
     local needsScreen = playerGui:WaitForChild("NeedsBar")
     local energyEventHandler = services.ReplicatedStorage.Needs.UpdateEnergy.OnClientEvent
     
-    services.ReplicatedStorage.ToolGiverRemotes.GiveTool:FireServer("Sleeping Bag"); task.wait(2)
-    localPlayer.Backpack:WaitForChild("Sleeping Bag").Parent = localPlayer.Character; task.wait(2)
+    services.ReplicatedStorage.ToolGiverRemotes.GiveTool:FireServer("Sleeping Bag")
+    task.wait(2)
+    localPlayer.Backpack:WaitForChild("Sleeping Bag").Parent = localPlayer.Character
+    task.wait(2)
     services.ReplicatedStorage.Tools.PlaceTool:FireServer(
         localPlayer.Character:FindFirstChild("Sleeping Bag"),
         localPlayer.Character.HumanoidRootPart.CFrame - Vector3.new(0,2,3.5),
         {}
-    ); task.wait(2)
+    )
     services.ReplicatedStorage.Bed.Anim:FireServer("sleep")
 
     repeat task.wait(1) until NEEDS.Energy == 1
     localPlayer.Character.Humanoid.Jump = true
     localPlayer.Character.Humanoid.Sit = false
-    task.wait(5)
-    services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
+    
+    repeat
+        task.wait(5)
+        services.ReplicatedStorage.SceptorTeleport:FireServer("New Royale")
+    until game.PlaceId == 1765700510
 end; sleep()
