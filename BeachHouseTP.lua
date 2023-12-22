6e7018237034072ac3dcb4616c6d0078b3ea9c69
aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaf <143303733+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaf@users.noreply.github.com>
2023-12-22 20:40:05 +0700

    Update BeachHouseTP.lua

--- BeachHouseTP.lua
+++ BeachHouseTP.lua
@@ -1,6 +1,5 @@
 local placeId = game.PlaceId
 repeat
     game:GetService("ReplicatedStorage").SceptorTeleport:FireServer("BeachHouse")
-    task.wait(5)
-    game:GetService("TeleportService"):Teleport(4728136927)
+    task.wait(1)
 until game.PlaceId ~= placeId
