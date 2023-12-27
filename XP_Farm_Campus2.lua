0a561c2d32540ca81329dd4c9264db209f83047a
Aeroxx12 <136855637+Aeroxx12@users.noreply.github.com>
2023-12-27 21:58:26 +0700

    Update XP_Farm_Campus2.lua

--- XP_Farm_Campus2.lua
+++ XP_Farm_Campus2.lua
@@ -6,15 +6,14 @@ Players.PlayerAdded:Connect(function() game:Shutdown() end)
 
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
-local _ = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
+local LP = Players.LocalPlayer
+for _,v in next, getconnections(LP.Idled) do v:Disable() end
+local _ = LP.Character or LP.CharacterAdded:Wait()
+
+coroutine.resume(coroutine.create(loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/CatpchaSolver.lua"))))
 
 local scripts = {
     ["735030788"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/BeachHouseTP.lua"),
     ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/BeachHouse.lua"),
     ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/School.lua")
 }; coroutine.resume(coroutine.create(loadstring(scripts[tostring(game.PlaceId)])))
-    
-if game.PlaceId ~= 735030788 then 
-    coroutine.resume(coroutine.create(loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixterity/RH/main/CatpchaSolver.lua"))))
-    coroutine.resume(coroutine.create(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/UtilityScripts/AntiAfk.lua"))))
-end
