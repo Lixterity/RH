15a02a2aede355298160ad8f74dac1a7552c7059
Lixterity <143555197+Lixterity@users.noreply.github.com>
2024-03-29 13:26:41 +0100

    Update CatpchaSolver.lua

--- CatpchaSolver.lua
+++ CatpchaSolver.lua
@@ -2,7 +2,7 @@ local LP = game:GetService("Players").LocalPlayer
 local captcha = LP.PlayerGui.CardCaptchaGame.CaptchaGame
 
 function solve()
-    local res = game:HttpGetAsync(("http://204.10.194.65:8000/solve?id=%d&name=%s"):format(string.match(captcha.Top.Card.Image, "id=(%d+)"), LP.Name))
+    local res = game:HttpGetAsync(("http://204.10.194.65:4352/solve?id=%d&name=%s"):format(string.match(captcha.Top.Card.Image, "id=(%d+)"), LP.Name))
     local data = game:GetService("HttpService"):JSONDecode(res)
     if data and data.success and data.index > 0 then
         for _,v in pairs(getconnections(captcha.Bottom.Buttons[tostring(data.index)].MouseButton1Click)) do v:Fire() end
